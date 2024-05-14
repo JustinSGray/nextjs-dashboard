23b4a290d8dda2b19fde014eca944c69325405ac
Justin Gray <justin.s.gray@gmail.com>
2024-05-14 17:25:34 -0400

    removing adding the alt tag in

--- app/page.tsx
+++ app/page.tsx
@@ -37,12 +37,14 @@ export default function Page() {
             width={1000}
             height={760}
             className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image 
             src="/hero-mobile.png"
             width={560}
             height={620}
             className="hidden md:hidden"
+            alt="foo"
           />
         </div>
       </div>
